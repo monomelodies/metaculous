8751f172596dde19ec3765003737f494ca18eaa2
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-16 10:04:06 +0200

    even better: negate so we can include hyphen (usually used to join words)

--- src/Parser.php
+++ src/Parser.php
@@ -72,7 +72,7 @@ class Parser
     {
         $text = strip_tags($text);
         $words = preg_split(
-            "@\W+@",
+            "@[^\w-]+@",
             $text
         );
         $cntlc = [];
